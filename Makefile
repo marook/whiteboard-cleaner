65a12cd26b74baffce743e5a732ee37bba997106
Markus Peröbner <markus.peroebner@gmail.com>
2014-04-21 13:56:15 +0200

    implemented source file if now destination is given feature

--- Makefile
+++ Makefile
@@ -8,3 +8,19 @@ install: ${PREFIX}/bin/whiteboard-cleaner
 
 ${PREFIX}/bin/whiteboard-cleaner: whiteboard-cleaner.sh
 	install -D -m 0755 "$^" "$@"
+
+.PHONY: test
+test: target/IMG_20140406_195919_a.png target/IMG_20140406_195919_b.png
+
+target/IMG_20140406_195919_a.png: test/IMG_20140406_195919.jpg
+	mkdir -p -- `dirname "$@"`
+	/bin/bash whiteboard-cleaner.sh "$^" "$@"
+
+target/IMG_20140406_195919_b.png: test/IMG_20140406_195919.jpg
+	mkdir -p -- `dirname "$@"`
+	cp "$^" "$@"
+	/bin/bash whiteboard-cleaner.sh "$@"
+
+.PHONY: clean
+clean:
+	rm -rf -- target
--- whiteboard-cleaner.sh
+++ whiteboard-cleaner.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 "$2"
+
+set -e
+
+#-----------------------------------------------------------------------
+# main
+
+SOURCE_IMAGE_PATH="$1"
+DEST_IMAGE_PATH="$2"
+
+if [ -z "${DEST_IMAGE_PATH}" ]
+then
+    WORK_IMAGE_PATH=`mktemp`
+else
+    WORK_IMAGE_PATH="${DEST_IMAGE_PATH}"
+fi
+
+convert "${SOURCE_IMAGE_PATH}" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 "${WORK_IMAGE_PATH}"
+
+if [ "${WORK_IMAGE_PATH}" != "${DEST_IMAGE_PATH}" ]
+then
+    mv "${WORK_IMAGE_PATH}" "${SOURCE_IMAGE_PATH}"
+fi
