e1dd050b58df5f1cfb99caff782d09d2a10b35e8
Markus Peröbner <markus.peroebner@gmail.com>
2014-04-21 14:35:54 +0200

    added color reduction support

--- Makefile
+++ Makefile
@@ -10,16 +10,16 @@ ${PREFIX}/bin/whiteboard-cleaner: whiteboard-cleaner.sh
 	install -D -m 0755 "$^" "$@"
 
 .PHONY: test
-test: target/IMG_20140406_195919_a.png target/IMG_20140406_195919_b.png
+test: target/IMG_20140406_195919_a.png target/IMG_20140406_195919_b.jpg
 
 target/IMG_20140406_195919_a.png: test/IMG_20140406_195919.jpg whiteboard-cleaner.sh
 	mkdir -p -- `dirname "$@"`
-	/bin/bash whiteboard-cleaner.sh "test/IMG_20140406_195919.jpg" "$@"
+	/bin/bash whiteboard-cleaner.sh -b "test/IMG_20140406_195919.jpg" "$@"
 
-target/IMG_20140406_195919_b.png: test/IMG_20140406_195919.jpg whiteboard-cleaner.sh
+target/IMG_20140406_195919_b.jpg: test/IMG_20140406_195919.jpg whiteboard-cleaner.sh
 	mkdir -p -- `dirname "$@"`
 	cp "test/IMG_20140406_195919.jpg" "$@"
-	/bin/bash whiteboard-cleaner.sh "$@"
+	/bin/bash whiteboard-cleaner.sh -b "$@"
 
 .PHONY: clean
 clean:
--- whiteboard-cleaner.sh
+++ whiteboard-cleaner.sh
@@ -1,14 +1,53 @@
 #!/bin/bash
 
 MAX_IMAGE_SIZE=1000x1000
+BLACK_AND_WHITE=0
 
 set -e
+
+usage(){
+    cat << EOF
+usage: $0 [options] INPUT_FILE [OUTPUT_FILE]
+
+Clean up photographs of whiteboard or paper notes.
+
+OPTIONS:
+   -h      Show this message
+   -b      'Black and White' image
+EOF
+}
 
 #-----------------------------------------------------------------------
 # main
 
+while getopts "hb" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 0
+             ;;
+         b)
+             BLACK_AND_WHITE=1
+             ;;
+         ?)
+             usage
+             exit 1
+             ;;
+     esac
+done
+
+# removed arguments parsed by getopts from argument list
+shift $(( $OPTIND -1 ))
+
 SOURCE_IMAGE_PATH="$1"
 DEST_IMAGE_PATH="$2"
+
+if [ -z "${SOURCE_IMAGE_PATH}" ]
+then
+    usage
+    exit 1
+fi
 
 if [ -z "${DEST_IMAGE_PATH}" ]
 then
@@ -17,7 +56,16 @@ else
     WORK_IMAGE_PATH="${DEST_IMAGE_PATH}"
 fi
 
-convert "${SOURCE_IMAGE_PATH}" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 -resize "${MAX_IMAGE_SIZE}>" "${WORK_IMAGE_PATH}"
+POST_PROCESSING=
+
+if [ "${BLACK_AND_WHITE}" == '1' ]
+then
+    POST_PROCESSING="-type Grayscale -colors 8"
+else
+    POST_PROCESSING="-colors 64"
+fi
+
+convert "${SOURCE_IMAGE_PATH}" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 -resize "${MAX_IMAGE_SIZE}>" ${POST_PROCESSING} "${WORK_IMAGE_PATH}"
 
 if [ "${WORK_IMAGE_PATH}" != "${DEST_IMAGE_PATH}" ]
 then
